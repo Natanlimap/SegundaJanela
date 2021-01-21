106357d85a7378f54473c222680804511333a5e4
Natanlimap <natanlimap@gmail.com>
2021-01-21 11:19:18 -0300

    Mais meta tags adicionadas

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,33 @@ const Home = () => (
         src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
         crossorigin></script>
       
-      <title>Segunda Janela</title>
-      <link rel="canonical" href="https://segundajanela.com.br"></link>
+      <title>Banda Segunda Janela</title>
+      <link rel="canonical" href="http://segundajanela.com.br"></link>
       <meta name='description' content='Banda potiguar independente formada em 2020 que mistura influências de rock indie, pop, pop-rock, rock alternativo pra formar a própria identidade musical.' />
       <meta name='robots' content='index, follow' />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta charset='UTF-8' />
+      <meta httpEquiv='content-language' content='pt-br'/>
+      <meta httpEquiv='content-type' content='text/html' charset='UTF-8'/>
+      <meta name="abstract" content="EP Meio do caminho disponível em todas as plataformas digitais"/>
+      <meta name="keywords" content=" mpb rock  musica  arte   pop       spotify indie   lançamento guitarra banda baixista bateria  artistas independentes  indie   musica nova brasil segundajanela seu olhar por ai vestido xadrez azul janela banda potiguar brasileira"/>
+      <meta name="robot" content="All"/>
+      <meta name="rating" content="general"/>
+      <meta name="distribution" content="global"/>
+      <meta name="language" content="PT"/>
+
+     
+      <meta name='author' content='Natan de Lima'/>
+      <meta name='creator' content='Natan de Lima'/>
+  
+      <meta property='og:locale' content='pt_BR'/>
+      <meta property='og:type' content='website'/>
+      <meta property='og:title' content='Banda Segunda Janela' />
+      <meta name='og:description' content='Banda potiguar independente formada em 2020 que mistura influências de rock indie, pop, pop-rock, rock alternativo pra formar a própria identidade musical.' />
+      <meta property='og:url' content='http://segundajanela.com.br/' />
+  
       <meta name="google-site-verification" content="cw-yW_6fseH4-gTGtxZ5HVW4G3HXnRAEvYYV0uAlBtk" />
+
     </Head>
 
     <main className='h-screen'>
@@ -37,3 +58,5 @@ const Home = () => (
 );
 
 export default Home;
+
+
