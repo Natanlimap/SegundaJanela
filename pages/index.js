9be85644dccdb1f7e6f38e1bbe397359ef2a36fd
Natanlimap <natanlimap@gmail.com>
2021-01-21 08:31:52 -0300

    searchconsole

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ const Home = () => (
       <meta name='description' content='Banda potiguar independente formada em 2020 que mistura influências de rock indie, pop, pop-rock, rock alternativo pra formar a própria identidade musical.'/>
       <meta name='robots' content='index, follow'/>
       <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+      <meta name="google-site-verification" content="pWyEaul4oGNcVA1DwBqYn-TUiApcapdtdKXoWJFsG7Y" />
     </Head>
     <h1>oi</h1>
   </div>
