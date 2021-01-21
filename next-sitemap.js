86c91e71d378303242c06d1940795ac00436fda6
Natanlimap <natanlimap@gmail.com>
2021-01-21 10:57:02 -0300

    adicionando xml

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,30 @@
 module.exports = {
-    siteUrl: 'https://segundajanela.com.br',
+    siteUrl: 'https://segundajanela.com.br/',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
     generateRobotsTxt: true,
-}
+    exclude: [],
+    // Default transformation function
+    transform: (config, path) => {
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      }
+    },
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+      additionalSitemaps: [
+        'https://segundajanela.com.br/sobre',
+        'https://segundajanela.com.br/meiodocaminho',
+        'https://segundajanela.com.br/contato',
+      ],
+    },
+  }
