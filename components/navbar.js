5de8ff3ee2c83b86885248f08a249a571cba5816
Natanlimap <natanlimap@gmail.com>
2021-01-21 21:22:00 -0300

    Musicas feito

--- components/navbar.js
+++ components/navbar.js
@@ -5,21 +5,21 @@ export default function NavBar() {
     return (
         <div>
             <nav className="nav flex flex-wrap items-center justify-between px-4">
-                 <div className="flex flex-no-shrink items-center mr-6 py-3 text-color">
+                <div className="flex flex-no-shrink items-center mr-6 py-3 text-color">
                     <div className='mx-2'>
-                    <Image
-                        src='/logo.png'
-                        width={23.91}
-                        height={33}
-                        alt='Logo da Banda Segunda Janela. Uma Janela de duas portas com um arco curvado em cima.'
-                    />
+                        <Image
+                            src='/logo.png'
+                            width={23.91}
+                            height={33}
+                            alt='Logo da Banda Segunda Janela. Uma Janela de duas portas com um arco curvado em cima.'
+                        />
                     </div>
-                </div> 
+                </div>
 
-                 <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
-                <label className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none"  for="menu-btn">
+                <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
+                <label className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none" for="menu-btn">
                     <span className="navicon bg-grey-darkest flex items-center relative"></span>
-                </label> 
+                </label>
 
                 <ul className="menu  md:border-none flex justify-end list-reset m-0 w-full md:w-auto">
                     <li className=" md:border-none">
@@ -27,7 +27,7 @@ export default function NavBar() {
                     </li>
 
                     <li className=" md:border-none">
-                    <Link href="/meiodocaminho"><a href="#" className="block md:inline-block px-4 py-3 no-underline text-color hover:text-grey-darker">Sobre nós</a></Link>
+                        <Link href="/meiodocaminho"><a href="#" className="block md:inline-block px-4 py-3 no-underline text-color hover:text-grey-darker">Meio do caminho</a></Link>
                     </li>
 
                     <li className=" md:border-none">
